02f84978b61efd867a0b4aceb400cbbbcb0d758c
Simone <sisilopes20@gmail.com>
2026-02-15 17:21:30 +0000

    config: add swagger

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import cors from "cors";
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yaml';
+import fs from 'fs';
+
+
 import { router } from "./routes";
 
 const app = express();
@@ -9,6 +14,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
+
+const swaggerDocument = YAML.parse(
+  fs.readFileSync('./swagger.yaml', 'utf8')
+);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
   if (error instanceof Error) {
